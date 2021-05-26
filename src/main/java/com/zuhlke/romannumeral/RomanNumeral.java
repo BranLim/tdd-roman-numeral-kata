c3f07923f8edd92f7baefbb70cef413a4b1b223d
Brandon Lim <brandon.lim@zuhlke.com>
2021-05-26 21:17:57 +0800

    Initial commit of Roman Numeral Kata

---
+++ src/main/java/com/zuhlke/romannumeral/RomanNumeral.java
@@ -0,0 +1,31 @@
+package com.zuhlke.romannumeral;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class RomanNumeral {
+
+    private Map<Integer, String> romanNumerals = new LinkedHashMap<>();
+
+    public RomanNumeral() {
+        romanNumerals.put(50, "L");
+        romanNumerals.put(40, "XL");
+        romanNumerals.put(10, "X");
+        romanNumerals.put(9, "IX");
+        romanNumerals.put(5, "V");
+        romanNumerals.put(4, "IV");
+        romanNumerals.put(1, "I");
+    }
+
+    public String convert(int number) {
+
+        StringBuilder romanNumber = new StringBuilder();
+        for (Map.Entry<Integer, String> entry : romanNumerals.entrySet()) {
+            while (number >= entry.getKey()){
+                romanNumber.append(entry.getValue());
+                number -= entry.getKey();
+            }
+        }
+        return romanNumber.toString();
+    }
+}
---
+++ src/test/java/com/zuhlke/romannumeral/RomanNumeralTest.java
@@ -0,0 +1,85 @@
+package com.zuhlke.romannumeral;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RomanNumeralTest {
+
+    @Test
+    void testConvert1ShouldReturnI() {
+        RomanNumeral romanNumeral = new RomanNumeral();
+        String convert = romanNumeral.convert(1);
+        assertEquals("I", convert);
+    }
+
+    @Test
+    void testConvert2ShouldReturnII() {
+        RomanNumeral romanNumeral = new RomanNumeral();
+        String convert = romanNumeral.convert(2);
+        assertEquals("II", convert);
+    }
+
+    @Test
+    void testConvert3ShouldReturnIII() {
+        RomanNumeral romanNumeral = new RomanNumeral();
+        String convert = romanNumeral.convert(3);
+        assertEquals("III", convert);
+    }
+
+    @Test
+    void testConvert4ShouldReturnIV() {
+        RomanNumeral romanNumeral = new RomanNumeral();
+        String convert = romanNumeral.convert(4);
+        assertEquals("IV", convert);
+    }
+
+    @Test
+    void testConvert5ShouldReturnV() {
+        RomanNumeral romanNumeral = new RomanNumeral();
+        String convert = romanNumeral.convert(5);
+        assertEquals("V", convert);
+    }
+
+    @Test
+    void testConvert9ShouldReturnIX() {
+        RomanNumeral romanNumeral = new RomanNumeral();
+        String convert = romanNumeral.convert(9);
+        assertEquals("IX", convert);
+    }
+
+    @Test
+    void testConvert10ShouldReturnX() {
+        RomanNumeral romanNumeral = new RomanNumeral();
+        String convert = romanNumeral.convert(10);
+        assertEquals("X", convert);
+    }
+
+    @Test
+    void testConvert14ShouldReturnXIV() {
+        RomanNumeral romanNumeral = new RomanNumeral();
+        String convert = romanNumeral.convert(14);
+        assertEquals("XIV", convert);
+    }
+
+    @Test
+    void testConvert50ShouldReturnL() {
+        RomanNumeral romanNumeral = new RomanNumeral();
+        String convert = romanNumeral.convert(50);
+        assertEquals("L", convert);
+    }
+
+    @Test
+    void testConvert40ShouldReturnXL() {
+        RomanNumeral romanNumeral = new RomanNumeral();
+        String convert = romanNumeral.convert(40);
+        assertEquals("XL", convert);
+    }
+
+    @Test
+    void testConvert30ShouldReturnXXX() {
+        RomanNumeral romanNumeral = new RomanNumeral();
+        String convert = romanNumeral.convert(30);
+        assertEquals("XXX", convert);
+    }
+}
