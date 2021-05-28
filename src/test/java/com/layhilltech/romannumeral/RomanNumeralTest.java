d07fdf215d795a52454ed1967a1e6c8d87839bfb
Brandon Lim <brandon.lim@zuhlke.com>
2021-05-28 18:22:17 +0800

    Refactor the code.

--- src/test/java/com/layhilltech/romannumeral/RomanNumeralTest.java
+++ src/test/java/com/layhilltech/romannumeral/RomanNumeralTest.java
@@ -6,79 +6,81 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RomanNumeralTest {
 
+    private RomanNumeral romanNumeral = new RomanNumeral();
+
     @Test
-    void testConvert1ShouldReturnI() {
-        RomanNumeral romanNumeral = new RomanNumeral();
+    void convert1ShouldReturnI() {
+
         String convert = romanNumeral.convert(1);
         assertEquals("I", convert);
     }
 
     @Test
-    void testConvert2ShouldReturnII() {
-        RomanNumeral romanNumeral = new RomanNumeral();
+    void convert2ShouldReturnII() {
+
         String convert = romanNumeral.convert(2);
         assertEquals("II", convert);
     }
 
     @Test
-    void testConvert3ShouldReturnIII() {
-        RomanNumeral romanNumeral = new RomanNumeral();
+    void convert3ShouldReturnIII() {
+
         String convert = romanNumeral.convert(3);
         assertEquals("III", convert);
     }
 
     @Test
-    void testConvert4ShouldReturnIV() {
-        RomanNumeral romanNumeral = new RomanNumeral();
+    void convert4ShouldReturnIV() {
+
         String convert = romanNumeral.convert(4);
         assertEquals("IV", convert);
     }
 
     @Test
-    void testConvert5ShouldReturnV() {
-        RomanNumeral romanNumeral = new RomanNumeral();
+    void convert5ShouldReturnV() {
+
         String convert = romanNumeral.convert(5);
         assertEquals("V", convert);
     }
 
     @Test
-    void testConvert9ShouldReturnIX() {
-        RomanNumeral romanNumeral = new RomanNumeral();
+    void convert9ShouldReturnIX() {
+
         String convert = romanNumeral.convert(9);
         assertEquals("IX", convert);
     }
 
     @Test
-    void testConvert10ShouldReturnX() {
-        RomanNumeral romanNumeral = new RomanNumeral();
+    void convert10ShouldReturnX() {
+
         String convert = romanNumeral.convert(10);
         assertEquals("X", convert);
     }
 
     @Test
-    void testConvert14ShouldReturnXIV() {
-        RomanNumeral romanNumeral = new RomanNumeral();
+    void convert14ShouldReturnXIV() {
+
         String convert = romanNumeral.convert(14);
         assertEquals("XIV", convert);
     }
 
     @Test
-    void testConvert50ShouldReturnL() {
-        RomanNumeral romanNumeral = new RomanNumeral();
+    void convert50ShouldReturnL() {
+
         String convert = romanNumeral.convert(50);
         assertEquals("L", convert);
     }
 
     @Test
-    void testConvert40ShouldReturnXL() {
-        RomanNumeral romanNumeral = new RomanNumeral();
+    void convert40ShouldReturnXL() {
+
         String convert = romanNumeral.convert(40);
         assertEquals("XL", convert);
     }
 
     @Test
-    void testConvert30ShouldReturnXXX() {
-        RomanNumeral romanNumeral = new RomanNumeral();
+    void convert30ShouldReturnXXX() {
+
         String convert = romanNumeral.convert(30);
         assertEquals("XXX", convert);
     }
